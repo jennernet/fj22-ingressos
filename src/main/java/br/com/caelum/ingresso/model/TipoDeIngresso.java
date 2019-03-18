94044d2b9efeeec4dc005151a5551ad9cc69c84c
Jenner de Assis Moreira <jenner.moreira@cnj.jus.br>
2019-03-18 10:09:42 -0300

    TEste

---
+++ src/main/java/br/com/caelum/ingresso/model/TipoDeIngresso.java
@@ -0,0 +1,23 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+
+public enum TipoDeIngresso {
+	INTEIRO(new SemDesconto()),
+	ESTUDANTE(new Estudantes()),
+	BANCO(new DescontoParaBancos());
+	
+	private final Desconto desconto;
+	
+	TipoDeIngresso(Desconto desconto){
+		this.desconto = desconto;
+	}
+	
+	public BigDecimal aplicaDesconto(BigDecimal valor) {
+		return desconto.aplicarDescontoSobre(valor);
+	}
+	
+	public String getDescricao() {
+		return desconto.getDescricao();
+	}
+}
